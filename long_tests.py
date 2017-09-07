283ad5fd2b7f18a321e5438d190974836efe734a
David Tauraso <david318@me.com>
2017-09-07 12:43:50 -0700

    Initial Commit

---
+++ long_tests.py
@@ -0,0 +1,82 @@
+from p1 import *
+import unittest
+from random import randint
+import time
+
+class Tests(unittest.TestCase):
+
+    '''
+    def test_subtract(self):
+        print("Running subtraction tests.")
+        for i in range(0, 501):
+            for j in range(0, 501):
+                self.assertEqual(i - j, subtract(i, j))
+            print("    Finished i = ", i, " subtraction set")
+        print("Finished subtraction tests.")
+    '''
+    '''
+    def test_Divide(self):
+        print("Running division tests.")
+        for i in range(1, 501):
+            for j in range(1, 501):
+                self.assertEqual((i // j, i % j), Divide(i, j))
+            print("    Finished i = ", i, " division set")
+        print("Finished division tests.")
+    '''
+    '''
+    def test_exp(self):
+        print("Running exponent tests.")
+        for i in range(1, 50):
+            for j in range(1, 50):
+                self.assertEqual(i**j, bin2dec(exp(dec2bin(i), dec2bin(j))))
+            print("    Finished i = ", i, " exponent set")
+        print("Finished division tests.")
+    '''
+    '''
+    def test_problem1Calc(self):
+        print("Running problem1Calc tests.")
+        for i in range(0, 10):
+            a = randint(0, 100)
+            b = randint(0, 100)
+            c = randint(0, 100)
+            d = randint(0, 100)
+            print("    Testing ", a, "^", b, " - ", c, "^", d) 
+            start = time.time()
+            ourResult = problem1Calc(a, b, c, d)
+            end = time.time()
+            print("Got an answer of ", ourResult)
+            self.assertEqual((a**b) - (c**d), ourResult)
+            
+            print("    Finished test. Time taken: ", end - start)
+        print("Finished problem1Calc tests.")
+    '''
+    
+    def test_problem2Calc(self):
+        print("Running problem2Calc tests.")
+        for i in range(0, 10):
+            a = randint(0, 1000)
+            b = randint(0, 1000)
+            c = randint(0, 1000)
+            d = randint(0, 1000)
+            print("    Testing ", a, "^", b, " / ", c, "^", d) 
+            start = time.time()
+            (ourQuotient, ourRemainder) = problem2Calc(a, b, c, d)
+            end = time.time()
+            print("Got a quotient of ", ourQuotient, " and a remainder of ", ourRemainder)
+            actualQuotient = (a**b)//(c**d)
+            actualRemainder = (a**b)%(c**d)
+            self.assertEqual((actualQuotient, actualRemainder), (ourQuotient, ourRemainder))
+            
+            print("    Finished test. Time taken: ", end - start)
+        print("Finished problem1Calc tests.")
+    
+
+            
+
+if __name__ == '__main__':
+    unittest.main()
+    
+
+
+
+    
